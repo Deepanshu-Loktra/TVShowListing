69edf3d2f0f8a4a9bdacb5a8fc0431fcabdf052b
Deepanshu <deepanshu@loktra.com>
2018-09-12 20:28:02 +0530

    Added search, room, merger

--- app/src/main/java/com/loktra/tvshowapp/adapter/TVShowAdapter.java
+++ app/src/main/java/com/loktra/tvshowapp/adapter/TVShowAdapter.java
@@ -3,7 +3,9 @@ package com.loktra.tvshowapp.adapter;
 import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
@@ -12,16 +14,17 @@ import com.loktra.tvshowapp.databinding.TvShowItemsBinding;
 import com.loktra.tvshowapp.repository.responses.TVShowResponse;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowViewHolder> {
 
-    private ArrayList<TVShowResponse> tvShowResponseArrayList;
+    private List<TVShowResponse> tvShowResponseArrayList;
 
     public TVShowAdapter() {}
 
 
     //Receiving Updated TV Show List And Updating Recyclerview Data
-    public void setTVShowList(ArrayList<TVShowResponse> tvShowResponseArrayList) {
+    public void setTVShowList(List<TVShowResponse> tvShowResponseArrayList) {
         this.tvShowResponseArrayList = tvShowResponseArrayList;
         notifyDataSetChanged();
     }
@@ -30,13 +33,14 @@ public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowView
     @Override
     public TVShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         TvShowItemsBinding tvShowItemsBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.tv_show_items, parent, false);
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tv_show_items,parent,false);
         return new TVShowViewHolder(tvShowItemsBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TVShowViewHolder holder, int position) {
         holder.binding.setTvShowResponse(tvShowResponseArrayList.get(position));
-        holder.binding.executePendingBindings();
+        //holder.binding.executePendingBindings();
         Glide.with(holder.binding.getRoot().getContext())
                 .load(tvShowResponseArrayList.get(position).posterImage.medium)
                 .into(holder.binding.tvshowPoster);
